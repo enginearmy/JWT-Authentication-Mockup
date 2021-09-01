61f4a3fb0cf5befcdfb478a4990168ab455f02b7
Steve Hamilton <steve@enginearmy.com>
2021-09-01 01:24:29 -0400

    add failed scenario lines of code

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import dotEnv from 'dotenv'
 
-import { goodLoginCredentials, missingUserCredentials, badPasswordCredentials } from './mockLoginCredentials.js'
+import { goodLoginCredentials } from './mockLoginCredentials.js'
 import { signUpUser, loginUser } from './auth/index.js'
 import mockServerRequest from './mockServerRequest.js'
 
 dotEnv.config();
 
-const signUp_Authenticate_Authorize_Request = async()=> {
+const signUp_Authenticate_Authorize_Request = async(loginCredentials)=> {
 
     await signUpUser(goodLoginCredentials); 
-    const userToken = await loginUser(badPasswordCredentials)  //User logs in and recieves a valid JWT
+    const userToken = await loginUser(loginCredentials)  //User logs in and recieves a valid JWT
     mockServerRequest(userToken);                              //User requests the resource using the JWT as authorization
 } 
 
-signUp_Authenticate_Authorize_Request();
+signUp_Authenticate_Authorize_Request(goodLoginCredentials); //Successful 
+
+// Bad credentials with invalid password should fail ( Uncomment to run )
+// signUp_Authenticate_Authorize_Request(badLoginCredentials)
